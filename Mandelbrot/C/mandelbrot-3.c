7ed58c9bf0e46c854b7d2be8857fabe6c28c43fa
AdamSimpson <adambsimpson@gmail.com>
2017-06-11 13:59:22 -0400

    moop

--- Mandelbrot/C/mandelbrot-3.c
+++ Mandelbrot/C/mandelbrot-3.c
@@ -8,7 +8,7 @@
 // Convert HSV [0,1] to RGB [0,1]
 void HSVtoRGB(float H, float S, float V, unsigned char *pixel) {
   // convert hue to degrees
-  H = H*360.0;
+  H *= 360.0;
 
   if(S <= 0.0) {
     pixel[0] = (unsigned char)(V * 255.0);
@@ -87,7 +87,7 @@ void CalculateColor(double continuous_dwell, double distance, int max_iterations
   const int dwell = floor(continuous_dwell);
   const double final_rad = continuous_dwell - (double)dwell;
 
-  // Point is within Mandelbrot set
+  // Point is within Mandelbrot set, color white
   if(dwell >= max_iterations) {
     H = 0.0;
     S = 0.0;
@@ -97,7 +97,6 @@ void CalculateColor(double continuous_dwell, double distance, int max_iterations
   }
 
   // log scale distance
-  // pixel spacing / 2.0 ?
   const double dist_scaled = log2(distance / pixel_spacing);
 
   // Convert scaled distance to Value in 8 intervals
@@ -110,7 +109,7 @@ void CalculateColor(double continuous_dwell, double distance, int max_iterations
   }
 
   // log scale dwell
-  double dwell_scaled = log(dwell)/log(100000);
+  double dwell_scaled = log(dwell)/log(100*max_iterations);
 
   // Remap the scaled dwell onto Hue and Saturation
   if(dwell_scaled < 0.5) {
@@ -124,21 +123,20 @@ void CalculateColor(double continuous_dwell, double distance, int max_iterations
   }
 
   // Lighten every other stripe
-  // Try multiplying by 0.90, 0.8
   if(dwell%2) {
-    V *= 0.85;
-    S *= 0.667;
+    V *= 0.90;
+    S *= 0.8;
   }
 
   // Break the stripes up depending on angle of orbit at escape
-  // if(final_angle > M_PI) {
-  //   H += 0.02;
-  // }
+  if(final_angle > M_PI) {
+    H += 0.02;
+   }
 
   // Break square into full color gradient
   H += 0.0001 * final_rad;
 
-  // Go around color wheel 10x to cover range 1->10000
+  // Go around color wheel 10x to cover range 1->max_iterations
   H *= 10.0;
   H -= floor(H);
   S -= floor(S);
@@ -151,16 +149,16 @@ void CalculateColor(double continuous_dwell, double distance, int max_iterations
 // https://www.mrob.com/pub/muency/distanceestimator.html
 // https://www.mrob.com/pub/muency/continuousdwell.html
 void CalculatePixel(double xO, double yO, double pixel_size, unsigned char *pixel) {
-  const int max_iterations = 2000;
+  const int max_iterations = 100;
 
   double x=0.0,y=0.0;
   double dx=0.0,dy=0.0;
   int iteration = 0;
-  const double escape_radius = 1000; // Increased from 2.0 to find better distance estimate
+  const double escape_radius = 10000; // Increased from 2.0 to find better distance estimate
   double distance = 0.0;
   double continuous_dwell = 0.0;
 
-  while( (x*x + y*y) < escape_radius && iteration < max_iterations) {
+  while( (x*x + y*y) < escape_radius*escape_radius && iteration < max_iterations) {
     // Iterate orbit
     const double x_new = x*x - y*y + xO;
     const double y_new = 2.0*x*y + yO;
@@ -180,11 +178,11 @@ void CalculatePixel(double xO, double yO, double pixel_size, unsigned char *pixe
   continuous_dwell = iteration;
 
   // Calculate the distance if the orbit escaped
-  if((x*x + y*y) >= escape_radius) {
+  if((x*x + y*y) >= escape_radius*escape_radius) {
     const double mag_z = sqrt(x*x + y*y);
     const double mag_dz = sqrt(dx*dx + dy*dy);
     distance = log(mag_z*mag_z) * mag_z / mag_dz;
-    continuous_dwell +=  log2(log2(mag_z)) - log2(log2(escape_radius));
+    continuous_dwell += log2(log2(mag_z)) - log2(log2(escape_radius));
   }
 
   // Calculate color based on dwell and distance
